5b252259946a9f4968c8c74916b73aad7571e59f
Ambar Dudhane <ambarsd12345@hotmail.com>
2025-05-21 16:20:58 -0500

    Disabled csrf. Added docker and yml file

--- src/main/java/com/ecom/ecommerce/config/SecurityConfiguration.java
+++ src/main/java/com/ecom/ecommerce/config/SecurityConfiguration.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -17,7 +18,9 @@ public class SecurityConfiguration {
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
-        httpSecurity.authorizeHttpRequests((requests) -> requests
+        httpSecurity
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests((requests) -> requests
                 .requestMatchers("/").permitAll()
                 .requestMatchers("/api/product/**").hasRole("USER")
                 .requestMatchers("/api/admin").hasRole("ADMIN")
